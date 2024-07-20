67c3cc0609ae422697f06d1e185affd8989d46a7
notintimetoguessthis123 <125166265+notintimetoguessthis123@users.noreply.github.com>
2024-07-20 01:19:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -11,4 +11,11 @@ document.addEventListener('DOMContentLoaded', function() {
             this.style.transform = 'scale(1)';
         });
     });
+
+    const menuIcon = document.querySelector('.menu-icon');
+    const navLinks = document.querySelector('.nav-links');
+
+    menuIcon.addEventListener('click', function() {
+        navLinks.classList.toggle('active');
+    });
 });
